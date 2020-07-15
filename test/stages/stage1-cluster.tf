045cb7d4c78612380a8a62151bc368949eddd0d1
seansund <seansund@users.noreply.github.com>
2020-07-15 04:42:17 +0000

    deploy: 5186805e87d0458647764285748730821c92841d

--- test/stages/stage1-cluster.tf
+++
@@ -1,8 +0,0 @@
-module "dev_cluster" {
-  source = "./module"
-
-  cluster_type            = var.cluster_type
-  login_user              = var.login_user
-  login_password          = var.login_password
-  server_url              = var.server_url
-}
