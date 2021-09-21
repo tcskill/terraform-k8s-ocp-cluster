7a066910594ba027ce789906b68646f60f6f6127
Thomas Skill <tskill@us.ibm.com>
2021-09-21 11:53:19 -0500

    yq updt

--- scripts/get-ingress-subdomain.sh
+++ scripts/get-ingress-subdomain.sh
@@ -8,7 +8,7 @@ mkdir -p "${OUT_DIR}"
 set -e
 
 INGRESS_SUBDOMAIN=$(oc get deployment router-default -n openshift-ingress -o yaml | \
-  yq r - 'spec.template.spec.containers[0].env(name==ROUTER_CANONICAL_HOSTNAME).value')
+  yq e.'spec.template.spec.containers[0].env(name==ROUTER_CANONICAL_HOSTNAME).value')  
 
 echo "Found ingress subdomain: ${INGRESS_SUBDOMAIN}"
 echo -n "${INGRESS_SUBDOMAIN}" > "${OUT_FILE}"
